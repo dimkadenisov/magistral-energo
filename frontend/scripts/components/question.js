db423cf1c9ab3f711130ca41335472481c8d8b90
Dima Denisov <2denisov@bk.ru>
2019-12-25 22:10:14 +0300

    add tooltip

--- frontend/scripts/components/question.js
+++ frontend/scripts/components/question.js
@@ -1,5 +1,37 @@
 $('.question').click(function() {
-	$(this)
-		.find('.answer')
-		.toggleClass('d-none');
+	if (
+		$(this)
+			.find('.answer')
+			.css('display') === 'none'
+	) {
+		$(this)
+			.find('.answer')
+			.show();
+		const popper = new Popper(this, $(this).find('.answer'), {
+			placement: 'bottom',
+			modifiers: {
+				flip: {
+					behaivor: [
+						'top',
+						'top-start',
+						'top-end',
+						'right',
+						'right-start',
+						'right-end',
+						'bottom',
+						'bottom-start',
+						'bottom-end',
+						'left',
+						'left-start',
+						'left-end',
+					],
+				},
+				offset: { enabled: true, offset: '0,10' },
+			},
+		});
+	} else {
+		$(this)
+			.find('.answer')
+			.hide();
+	}
 });
