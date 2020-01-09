40cf2e1aaf1d56348282120762baef9c25117681
Dima Denisov <2denisov@bk.ru>
2020-01-09 14:51:50 +0300

    remove comments

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -477,51 +477,6 @@ cssVars({
 });
 "use strict";
 
-// function updateSwiperOnBreakpoint({
-// 	swiperClass,
-// 	swiperConfig,
-// 	maxWidthsArray,
-// }) {
-// 	if ($('.' + swiperClass).length === 0) return false;
-// 	const breakpoints = [];
-// 	maxWidthsArray.forEach(item => {
-// 		breakpoints.push(window.matchMedia(`(max-width: ${item}px)`));
-// 		breakpoints.push(window.matchMedia(`(min-width: ${item + 1}px)`));
-// 	});
-// 	let swipers = generateSwipers({
-// 		swiperClass: swiperClass,
-// 		swiperConfig: swiperConfig,
-// 	});
-// 	const breakpointChecker = function() {
-// 		breakpoints.forEach(breakpoint => {
-// 			if (breakpoint.matches) {
-// 				swipers.forEach(item => {
-// 					item.destroy();
-// 				});
-// 				swipers = generateSwipers({
-// 					swiperClass: swiperClass,
-// 					swiperConfig: swiperConfig,
-// 				});
-// 			}
-// 		});
-// 	};
-// 	breakpoints.forEach(breakpoint => {
-// 		breakpoint.addListener(breakpointChecker);
-// 	});
-// 	breakpointChecker();
-// 	return swipers;
-// }
-// function updateSwiperOnBreakpoint(breakpoints, swipers) {
-// 	breakpoints.forEach(({ breakpoint, callback }) => {
-// 		breakpoint.addListener(() => {
-// 			if (breakpoint.matches) {
-// 				swipers.forEach(swiper => {
-// 					callback(swiper);
-// 				});
-// 			}
-// 		});
-// 	});
-// }
 function updateSwiperOnBreakpoint(breakpointsToDestroy, breakpointsToInit, swipers, swiperClass, swiperConfig) {
   breakpointsToDestroy.forEach(function (breakpoint) {
     window.matchMedia(breakpoint).addListener(function () {
