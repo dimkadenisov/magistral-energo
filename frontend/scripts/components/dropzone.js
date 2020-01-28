7a09f0258d59b1a3ad770c743c31d2f370ac08ca
Dima Denisov <2denisov@bk.ru>
2020-01-28 22:11:58 +0300

    dropzone & profile

--- frontend/scripts/components/dropzone.js
+++ frontend/scripts/components/dropzone.js
@@ -1,35 +1,8 @@
 const dropzoneForm = $('.dropzone-form');
 const dropzone = $('.dropzone');
 
-function checkFilesExtensions(files, extensions) {
-	return [...files].every(({ name }) => {
-		return extensions.includes(name.match(/.(\w*)$/)[1]);
-	});
-}
-
-function renderText(isMultiple, files) {
-	return !isMultiple && files.length > 1
-		? 'Можно загрузить только один файл'
-		: [...files].map(file => file.name).join('');
-}
-
 function showFiles(files, nodeToDisplay) {
-	const isMultiple = !!$(this)
-		.closest('.dropzone')
-		.find('.dropzone__input')
-		.attr('multiple');
-
-	const extensions = $(this)
-		.closest('.dropzone')
-		.find('.dropzone__input')
-		.attr('data-allowed-files-extensions')
-		.split(' ');
-
-	const text = checkFilesExtensions(files, extensions)
-		? renderText(isMultiple, files)
-		: `Можно загружать файлы только с расширениями ${extensions}`;
-
-	nodeToDisplay.text(text);
+	nodeToDisplay.text([...files].map(file => file.name).join(''));
 }
 
 dropzone
@@ -60,40 +33,40 @@ $('.dropzone__input').on('change', function(e) {
 
 dropzoneForm.on('submit', function(e) {
 	e.preventDefault();
-	console.log('ti pidoras');
 
-	// if (dropzoneForm.hasClass('dropzone_uploading')) return false;
+	const form = this;
 
-	// dropzoneForm.addClass('dropzone_uploading').removeClass('fropzone_error');
-	// e.preventDefault();
+	if ($(this).hasClass('dropzone_uploading')) return false;
 
-	// const ajaxData = new FormData(dropzoneForm.get(0));
+	$(this)
+		.addClass('dropzone_uploading')
+		.removeClass('dropzone_error');
+	dropzone.find('.dropzone-form__files-count').text('Загрузка');
 
-	// // if (droppedFiles) { // check this shit
-	// // 	$.each(droppedFiles, function(i, file) {
-	// // 		ajaxData.append($input.attr('name'), file);
-	// // 	});
-	// // }
-
-	// $.ajax({
-	// 	url: dropzoneForm.attr('action'),
-	// 	type: dropzoneForm.attr('method'),
-	// 	data: ajaxData,
-	// 	dataType: 'json',
-	// 	cache: false,
-	// 	contentType: false,
-	// 	processData: false,
-	// 	complete: function() {
-	// 		dropzoneForm.removeClass('dropzone_uploading');
-	// 	},
-	// 	success: function(data) {
-	// 		dropzoneForm.addClass(
-	// 			data.success === true ? 'dropzone_success' : 'dropzone_error',
-	// 		);
-	// 		// if (!data.success) $errorMsg.text(data.error); // check this shit
-	// 	},
-	// 	error: function() {
-	// 		console.error('Dropzone Error');
-	// 	},
-	// });
+	$.ajax({
+		url: $(this).attr('action'),
+		type: $(this).attr('method'),
+		data: new FormData(form),
+		dataType: 'json',
+		cache: false,
+		contentType: 'form/multipart',
+		complete: function() {
+			$(this).removeClass('dropzone_uploading');
+		}.bind(form),
+		success: function(data) {
+			$(this)
+				.find('.dropzone-form__files-count')
+				.text(
+					data.success === true
+						? 'Успешная загрузка'
+						: 'Ошибка загрузки файлов',
+				);
+		}.bind(form),
+		error: function() {
+			$(this)
+				.find('.dropzone-form__files-count')
+				.text('Ошибка загрузки файлов');
+			console.error('Dropzone Error');
+		}.bind(form),
+	});
 });
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -140,24 +140,10 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 var dropzoneForm = $('.dropzone-form');
 var dropzone = $('.dropzone');
 
-function checkFilesExtensions(files, extensions) {
-  return _toConsumableArray(files).every(function (_ref) {
-    var name = _ref.name;
-    return extensions.includes(name.match(/.(\w*)$/)[1]);
-  });
-}
-
-function renderText(isMultiple, files) {
-  return !isMultiple && files.length > 1 ? 'Можно загрузить только один файл' : _toConsumableArray(files).map(function (file) {
-    return file.name;
-  }).join('');
-}
-
 function showFiles(files, nodeToDisplay) {
-  var isMultiple = !!$(this).closest('.dropzone').find('.dropzone__input').attr('multiple');
-  var extensions = $(this).closest('.dropzone').find('.dropzone__input').attr('data-allowed-files-extensions').split(' ');
-  var text = checkFilesExtensions(files, extensions) ? renderText(isMultiple, files) : "\u041C\u043E\u0436\u043D\u043E \u0437\u0430\u0433\u0440\u0443\u0436\u0430\u0442\u044C \u0444\u0430\u0439\u043B\u044B \u0442\u043E\u043B\u044C\u043A\u043E \u0441 \u0440\u0430\u0441\u0448\u0438\u0440\u0435\u043D\u0438\u044F\u043C\u0438 ".concat(extensions);
-  nodeToDisplay.text(text);
+  nodeToDisplay.text(_toConsumableArray(files).map(function (file) {
+    return file.name;
+  }).join(''));
 }
 
 dropzone.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) {
@@ -178,36 +164,28 @@ $('.dropzone__input').on('change', function (e) {
 });
 dropzoneForm.on('submit', function (e) {
   e.preventDefault();
-  console.log('ti pidoras'); // if (dropzoneForm.hasClass('dropzone_uploading')) return false;
-  // dropzoneForm.addClass('dropzone_uploading').removeClass('fropzone_error');
-  // e.preventDefault();
-  // const ajaxData = new FormData(dropzoneForm.get(0));
-  // // if (droppedFiles) { // check this shit
-  // // 	$.each(droppedFiles, function(i, file) {
-  // // 		ajaxData.append($input.attr('name'), file);
-  // // 	});
-  // // }
-  // $.ajax({
-  // 	url: dropzoneForm.attr('action'),
-  // 	type: dropzoneForm.attr('method'),
-  // 	data: ajaxData,
-  // 	dataType: 'json',
-  // 	cache: false,
-  // 	contentType: false,
-  // 	processData: false,
-  // 	complete: function() {
-  // 		dropzoneForm.removeClass('dropzone_uploading');
-  // 	},
-  // 	success: function(data) {
-  // 		dropzoneForm.addClass(
-  // 			data.success === true ? 'dropzone_success' : 'dropzone_error',
-  // 		);
-  // 		// if (!data.success) $errorMsg.text(data.error); // check this shit
-  // 	},
-  // 	error: function() {
-  // 		console.error('Dropzone Error');
-  // 	},
-  // });
+  var form = this;
+  if ($(this).hasClass('dropzone_uploading')) return false;
+  $(this).addClass('dropzone_uploading').removeClass('dropzone_error');
+  dropzone.find('.dropzone-form__files-count').text('Загрузка');
+  $.ajax({
+    url: $(this).attr('action'),
+    type: $(this).attr('method'),
+    data: new FormData(form),
+    dataType: 'json',
+    cache: false,
+    contentType: 'form/multipart',
+    complete: function () {
+      $(this).removeClass('dropzone_uploading');
+    }.bind(form),
+    success: function (data) {
+      $(this).find('.dropzone-form__files-count').text(data.success === true ? 'Успешная загрузка' : 'Ошибка загрузки файлов');
+    }.bind(form),
+    error: function () {
+      $(this).find('.dropzone-form__files-count').text('Ошибка загрузки файлов');
+      console.error('Dropzone Error');
+    }.bind(form)
+  });
 });
 "use strict";
 
